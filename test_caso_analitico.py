28ebf310a069adf7d9f35863dfb9256334f5540f
Arturo Rodriguez <da.rodriguezh@uniandes.edu.co>
2020-12-20 00:51:51 -0500

    Resultados y gráficas actualziadas

--- test_caso_analitico.py
+++ test_caso_analitico.py
@@ -1,11 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-L = 100
+L = 50
 E = 2.1*10**6
-l = 5
+l = 0.1
 Z1 = 0.5
-sbarra = 100
+sbarra = 2100
 
 def ex_analitico(x,L,E,l,Z1,sbarra):
 	l0 = 1/2/l
@@ -14,7 +14,7 @@ def ex_analitico(x,L,E,l,Z1,sbarra):
 	return ebarra-ld*l/2*ebarra*(np.exp((ld*x*l-x)/l)+np.exp((ld*l*L-ld*l*x-L+x)/l))
 _X = np.linspace(0,L,1000)
 plt.plot(_X,ex_analitico(_X,L,E,l,Z1,sbarra))
-plt.xlim([-10,110])
+plt.grid()
 # plt.ylim([4.5*10**-5,6.5*10**-5])
 # plt.savefig('defx_analitico.png',transparent=True)
 plt.show()
